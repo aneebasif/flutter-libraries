65bee696a3346d37201e7af82aa7298e00f2246a
Saad Javaid Alvi <saadalvi@we-over-i.com>
2023-09-01 16:28:35 +0500

    created base button iwth icon and border radius option

---
+++ buttons/lib/capsule_button/base_capsule_button.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import 'capsule_icon_button.dart';
+
+/// An all in one capsule button
+class WoiBaseButton extends StatelessWidget {
+  const WoiBaseButton({
+    Key? key,
+    required this.text,
+    required this.onTap,
+    this.icon,
+    this.borderRadius = 50,
+    this.textStyle,
+    this.borderColor,
+    this.heigth,
+    this.width,
+    this.fillColor,
+    this.isDisabled = false,
+    this.iconLocation = IconLocation.start,
+  }) : super(key: key);
+  final double borderRadius;
+  final String? text;
+  final VoidCallback? onTap;
+  final TextStyle? textStyle;
+  final Color? borderColor;
+  final double? heigth;
+  final Color? fillColor;
+  final double? width;
+  final bool isDisabled;
+  final IconLocation iconLocation;
+  final Widget? icon;
+  final Color disabledColor = const Color(0xffD9D9D9);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: isDisabled ? null : onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: isDisabled ? disabledColor : fillColor ?? Colors.black,
+          borderRadius: BorderRadius.circular(borderRadius),
+          border: Border.all(
+            color: isDisabled ? disabledColor : borderColor ?? Colors.black,
+          ),
+        ),
+        height: heigth ?? 38,
+        width: width,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            iconLocation == IconLocation.start
+                ? icon ?? Container()
+                : Container(),
+            _textContainer(),
+            iconLocation == IconLocation.end
+                ? icon ?? Container()
+                : Container(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _textContainer() {
+    return Text(
+      text!,
+      overflow: TextOverflow.ellipsis,
+      style: textStyle ??
+          const TextStyle(
+            height: 1,
+            color: Colors.white,
+            fontSize: 11,
+          ),
+      textAlign: TextAlign.center,
+    );
+  }
+}
