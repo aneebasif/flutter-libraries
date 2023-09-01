fe8f0f40977345599a2ffa0c96f21fe3272e870d
Saad Javaid Alvi <saadalvi@we-over-i.com>
2023-09-01 13:03:50 +0500

    added comment for better understanding

--- buttons/capsule_button/lib/main.dart
+++ buttons/capsule_button/lib/main.dart
@@ -1,5 +1,6 @@
 library weoveri_button;
 
+/// List of all Capsule Buttons
 export 'capsule_button/simple_capsule_button.dart';
 export 'capsule_button/capsule_icon_button.dart';
 export 'capsule_button/loading_capsule_button.dart';
