18ca03c0781ed3c91d4183b4b4ef7e724abc79a0
Saad Javaid Alvi <saadalvi@we-over-i.com>
2023-09-01 12:55:13 +0500

    updated example for buttons

--- buttons/capsule_button/example/lib/main.dart
+++ buttons/capsule_button/example/lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:weoveri_capsule_button/capsule_button.dart';
 import 'package:flutter/material.dart';
+import 'package:weoveri_button/main.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,18 +10,55 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Capsule Button Example'),
+          title: const Text('Capsule Buttons'),
         ),
-        body: Center(
-          child: MyCapsuleButton(
-            text: 'button'.toUpperCase(),
-            onTap: () {},
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            WoiCapsuleButton(
+              text: 'button'.toUpperCase(),
+              onTap: () {},
+              width: 200,
+            ),
+            WoiCapsuleIconButton(
+              text: 'button'.toUpperCase(),
+              onTap: () {},
+              icon: const Padding(
+                padding: EdgeInsets.only(right: 8.0),
+                child: Icon(
+                  Icons.add_link,
+                  color: Colors.white,
+                ),
+              ),
+              iconLocation: IconLocation.start,
+              width: 200,
+            ),
+            WoiCapsuleLoadingButton(
+              onTap: () {},
+              circularProgressIndicator: const CircularProgressIndicator(),
+              circularProgressSize: 20,
+              width: 200,
+            ),
+            WoiCapsuleGradientButton(
+              onTap: () {},
+              width: 200,
+              text: 'Button',
+              gradient: const LinearGradient(
+                colors: [
+                  Colors.red,
+                  Colors.green,
+                ],
+              ),
+            ),
+            Row(),
+          ],
         ),
       ),
     );
