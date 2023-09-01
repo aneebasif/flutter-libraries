1843ea806c439af95e540b2a84c99346024d9859
Saad Javaid Alvi <saadalvi@we-over-i.com>
2023-09-01 12:17:33 +0500

    updated example project

--- buttons/capsule_button/example/lib/main.dart
+++ buttons/capsule_button/example/lib/main.dart
@@ -7,38 +7,21 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Demo Home Page'),
-      ),
-      body: Center(
-        child: MyCapsuleButton(
-          text: 'button',
-          onTap: () {},
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Capsule Button Example'),
+        ),
+        body: Center(
+          child: MyCapsuleButton(
+            text: 'button'.toUpperCase(),
+            onTap: () {},
+          ),
         ),
       ),
     );
