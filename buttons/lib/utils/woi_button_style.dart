3ef5c8ba0f617963015649f192a76e9e6e062317
Saad Javaid Alvi <saadalvi@we-over-i.com>
2023-09-15 16:42:22 +0500

    saperated button style file

---
+++ buttons/lib/utils/woi_button_style.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:weoveri_button/main.dart';
+
+class WoiButtonStyle {
+  TextStyle? textStyle;
+  BorderRadius? borderRadius;
+  Border? border;
+  Widget? sideWidget;
+  double? sideWidgetSize;
+  WidgetLocation widgetLocation;
+  String? text;
+  Color? backgroundColor;
+  List<BoxShadow>? boxShadow;
+  Gradient? gradient;
+  double? height;
+  double? width;
+  EdgeInsets? textMargin;
+  WoiButtonStyle({
+    this.textStyle,
+    this.borderRadius,
+    this.border,
+    this.sideWidget,
+    this.sideWidgetSize,
+    this.widgetLocation = WidgetLocation.start,
+    this.text,
+    this.backgroundColor,
+    this.boxShadow,
+    this.gradient,
+    this.height,
+    this.width,
+    this.textMargin,
+  });
+}
