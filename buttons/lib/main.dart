f463ec4895e3ebcfa905694b3eca9d15dbe2e25a
Saad Javaid Alvi <saadalvi@we-over-i.com>
2023-09-15 16:58:06 +0500

    updated the export files

--- buttons/lib/main.dart
+++ buttons/lib/main.dart
@@ -1,11 +1,10 @@
 library weoveri_button;
 
-/// List of all Capsule Buttons
-export 'capsule_button/simple_capsule_button.dart';
-export 'capsule_button/capsule_icon_button.dart';
-export 'capsule_button/loading_capsule_button.dart';
-export 'capsule_button/capsule_gradient_button.dart';
-export 'capsule_button/base_capsule_button.dart';
+// Text Button
+export 'buttons/text_button.dart';
+
+// Text Button
+export 'utils/woi_button_style.dart';
 
 // Parallalogram Button
 export 'parallalogram_button/parallalogram_button.dart';
